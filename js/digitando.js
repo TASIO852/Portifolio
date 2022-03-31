5e8c2ec0a7a867cbcee762659bf3da9e32462d55
TASIO BRENO SANTOS GUIMARAES <73203739+TASIO852@users.noreply.github.com>
2022-03-30 22:25:28 -0300

    Primeira versao do site V0.1.0
    Proxima versao sera feito o Backend em python

---
+++ js/digitando.js
@@ -0,0 +1,19 @@
+var content = 'Vamos Crescer de forma conjunta';
+var text = document.getElementById('text');
+
+var speed = 250;
+var cont = 0;
+
+function typeWriter() {
+    if (cont < content.length) {
+        text.textContent += content.charAt(cont);
+        cont++;
+        setTimeout(typeWriter, speed);
+    } else {
+        text.textContent = '';
+        cont = 0;
+        typeWriter();
+    }
+}
+
+typeWriter();
