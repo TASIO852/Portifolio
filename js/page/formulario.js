1ebea243152fe4eae70e3b0c2305aaf6ceaaed7f
TASIO852 <tarsiosantos@outlook.com>
2023-02-02 18:51:21 -0300

    Bugs css e Update form

--- js/page/formulario.js
+++ js/page/formulario.js
@@ -1,82 +1,76 @@
-$(function() {
-    $('#form').submit(function(e) {
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
 
-        e.preventDefault();
-        var Name = $('input[Name=Nome]').val();
-        var Telefone = $('input[Name=Telefone]').val();
-        var Email = $('input[Name=Email]').val();
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == x.length - 1) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n);
+}
 
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
 
-        if (checkName(Name) == false) {
-            applyInvalidField($('input[Name=Nome]'));
-        }
-
-        if (checkTelefone(Telefone) == false) {
-            applyInvalidField($('input[Name=Telefone]'));
-        }
-
-        if (checkEmail(Email) == false) {
-            applyInvalidField($('input[Name=Email]'));
-        } else {
-            alert('Formulário enviado com sucesso');
-        }
-
-    });
-    // bug preenchimento automatico (.focus)
-    $('input[type=text]').focus(function() {
-        resetInvalidField($(this));
-    });
-    // melhorar RegExp
-    function checkName(Name) {
-        if (Name == ' ') {
-            return false;
-        }
-        var contar = Name.split(' ').length;
-        var splitStr = Name.split(' ');
-
-        if (contar >= 2) {
-            for (var i = 0; i < contar; i++) {
-                if (splitStr[i].match(/^[A-Za-z]{1,}[A-Za-z]{1,}$/)) {
-
-                } else {
-                    return false;
-                }
-            }
-        } else {
-            return false;
-        }
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x,
+    y,
+    i,
+    valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
     }
-    // melhorar RegExp
-    function checkTelefone(Telefone) {
-        if (Telefone == ' ') {
-            return false;
-        }
-        if (Telefone.match(/^[0-9]{11}$/) == null) {
-            return false;
-        }
-    }
-    // melhorar RegExp
-    function checkEmail(Email) {
-        if (Email == ' ') {
-            return false;
-        }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
 
-        if (Email.match(/^([a-z0-9-_]{1,})+@+([a-z.]{1,})$/) == null) {
-            return false;
-        }
-
-    }
-
-    function applyInvalidField(el) {
-        el.css('color', 'red');
-        el.css('border', '2px solid red');
-        el.val('invalid camp');
-    }
-
-    function resetInvalidField(el) {
-        el.css('color', 'gray');
-        el.css('border', '1px solid gray');
-        el.val('');
-    }
-
-});
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i,
+    x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
+}
